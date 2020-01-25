863bed64c269379bd0e2f5c4686e0e976341876d
krhubbard2 <krhubbard2@alaska.edu>
2020-01-25 13:40:11 -0900

    Created main.cpp, stopwatch.cpp, stopwatch.hpp

---
+++ Hw001/TimeIt/stopwatch.cpp
@@ -0,0 +1,6 @@
+// Kelby Hubbard
+// CS202
+// Jan. 26, 2020
+// HW001 -- Time It I
+
+#include "stopwatch.hpp"
---
+++ Hw001/TimeIt/stopwatch.hpp
@@ -0,0 +1,25 @@
+// Kelby Hubbard
+// CS202
+// Jan. 26, 2020
+// HW001 -- Time It I
+
+#ifndef STOPWATCH_HPP_
+#define STOPWATCH_HPP_
+
+
+class StopWatch
+{
+public:
+
+
+
+private:
+
+};
+
+
+
+
+
+
+#endif
