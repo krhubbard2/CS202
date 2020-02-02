4d5f4a90af84a55011fe0a44dda815bfc5b9a81a
krhubbard2 <krhubbard2@alaska.edu>
2020-02-02 12:15:09 -0900

    Created outline of struct Value

---
+++ Hw003/LinkedList/main.cpp
@@ -0,0 +1,47 @@
+// Kelby Hubbard
+// CS202
+// Feb. 2, 2020
+// Hw003 -- Linked List with shared_ptr<T>
+
+#include <iostream>
+using std::cout;
+using std::endl;
+#include <list>
+using std::list;
+#include <memory>
+using std::make_shared;
+using std::shared_ptr;
+#include <algorithm>
+using std::find;
+
+struct Value
+{
+  //Queue: First-in First-out
+  int pushqueue;
+  int popqueue;
+
+  //Stack: Last-in First-out
+  int pushstack;
+  int popstack;
+
+  //Insert and Find
+
+
+  //Print List
+
+};
+
+int main()
+{
+
+
+
+
+
+
+
+
+
+  return 0;
+
+}
