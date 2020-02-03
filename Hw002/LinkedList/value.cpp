8b3d183b096c35f107ce50a8bbbd09cb2a02a28f
Kelby <krhubbard2@alaska.edu>
2020-02-02 19:42:45 -0900

    Implamented popQueue in value.cpp

--- Hw002/LinkedList/value.cpp
+++ Hw002/LinkedList/value.cpp
@@ -2,3 +2,15 @@
 // CS202
 // Feb. 2, 2020
 // Hw002 -- Linked List with shared_ptr<T>
+
+#include "value.hpp"
+
+void pushQueue(list<shared_ptr<Value>>& list, const Value& dat)
+{
+  list.push_back(make_shared<Value>(dat));
+}
+
+void popQueue(list<shared_ptr<Value>>& list)
+{
+  list.pop_front();
+}
