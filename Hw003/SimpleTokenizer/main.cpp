3c875775900031a999f37c5e33481c3d21d46df4
Kelby <krhubbard2@alaska.edu>
2020-02-07 11:56:52 -0900

    Created basis of SimpleTokenizer

---
+++ Hw003/SimpleTokenizer/main.cpp
@@ -0,0 +1,23 @@
+// Kelby Hubbard
+// CS202
+// February 16, 2020
+// Hw003 -- A Simple Tokenizer
+
+#include "tokenizer.hpp"
+
+int main()
+{
+
+
+
+
+
+
+
+
+
+
+
+
+  return 0;
+}
---
+++ Hw003/SimpleTokenizer/tokenizer.hpp
@@ -0,0 +1,48 @@
+// Kelby Hubbard
+// CS202
+// February 16, 2020
+// Hw003 -- A Simple Tokenizer
+
+#ifndef TOKENIZER_HPP_
+#define TOKENIZER_HPP_
+
+/////////////////////////////////////////////////////////////////////////
+// U S I N G ////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+#include <string>
+using std::string;
+#include <vector>
+using std::vector;
+#include <sstream>
+using std::istringstream;
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////
+// P R O T O T Y P E S //////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+
+bool LineToTokens(const std::string& line, std::vector<std::string>& tokens);
+
+bool ReadLine(std::istream& is, std::vector<std::string>& tokens,
+          std::vector<std::pair<int, int>>& linecols);
+
+void PrintTokens(std::ostream& os, const std::vector<std::string>& tokens,
+          const std::vector<std::pair<int, int>>& linecols);
+
+
+
+
+
+
+
+#endif
