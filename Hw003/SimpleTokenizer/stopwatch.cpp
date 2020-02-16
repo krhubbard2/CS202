17fb9aa181a92a7f3e2840e01ba170fdacc57068
Kelby <krhubbard2@alaska.edu>
2020-02-16 14:36:32 -0900

    Added stopwatch.cpp/.hpp for timing

---
+++ Hw003/SimpleTokenizer/stopwatch.cpp
@@ -0,0 +1,26 @@
+// Kelby Hubbard
+// CS202
+// Jan. 26, 2020
+// HW001 -- Time It II
+
+#include "stopwatch.hpp"
+
+
+void StopWatch::starttimer()
+{
+  _start = std::chrono::system_clock::now();
+}
+
+void StopWatch::stoptimer()
+{
+  _end = std::chrono::system_clock::now();
+}
+
+void StopWatch::elapsed()
+{
+  std::chrono::duration<double> elapsed_seconds = _end-_start;
+  std::time_t end_time = std::chrono::system_clock::to_time_t(_end);
+
+  std::cout << "Finished job: " << std::ctime(&end_time)
+       << "Elapsed time: " << elapsed_seconds.count() << "s\n";
+}
