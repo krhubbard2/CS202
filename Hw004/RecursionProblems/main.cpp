53109d428b8913c10e27d2c2bbce935404051928
Kelby <krhubbard2@alaska.edu>
2020-03-07 13:26:26 -0900

    Started basis. Catch will test for correct fib numbers.

---
+++ Hw004/RecursionProblems/main.cpp
@@ -0,0 +1,35 @@
+// Kelby Hubbard
+// CS202
+// March 8, 2020
+// Hw004 -- Recursion Problems
+#include <catch2/catch.hpp>
+#define CATCH_CONMFIG_MAIN
+
+int fib(int n)
+{
+  //
+
+  return 0;
+}
+
+int factorial(int n)
+{
+  //
+
+  return 0;
+}
+
+int ack(int m, int n)
+{
+  //
+
+  return 0;
+}
+
+TEST_CASE("Fibonacci Sequence", "[Fibonacci]")
+{
+    REQUIRE(fib(0) == 0);
+    REQUIRE(fib(1) == 1);
+    REQUIRE(fib(9) == 34);
+    REQUIRE(fib(14) == 377);
+}
