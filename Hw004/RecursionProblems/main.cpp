cf42aca9133dec7e51c6b7d9078c4b1ab04c015b
Kelby <krhubbard2@alaska.edu>
2020-03-07 13:35:45 -0900

    Implemented fib(n) with recursion

--- Hw004/RecursionProblems/main.cpp
+++ Hw004/RecursionProblems/main.cpp
@@ -2,34 +2,54 @@
 // CS202
 // March 8, 2020
 // Hw004 -- Recursion Problems
+// #include <catch2/catch.hpp>
+// #include <iostream>
+// #define CATCH_CONFIG_MAIN
+//
+// int fib(int n)
+// {
+//   //
+//
+//   return 0;
+// }
+//
+// int factorial(int n)
+// {
+//   //
+//
+//   return 0;
+// }
+//
+// int ack(int m, int n)
+// {
+//   //
+//
+//   return 0;
+// }
+//
+// TEST_CASE("Fibonacci Sequence", "[Fibonacci]")
+// {
+//     REQUIRE( fib(0) == 0 );
+//     REQUIRE( fib(1) == 1 );
+//     REQUIRE( fib(9) == 34 );
+//     REQUIRE( fib(14) == 377 );
+// }
+
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include <catch2/catch.hpp>
-#define CATCH_CONMFIG_MAIN
 
-int fib(int n)
-{
-  //
-
-  return 0;
+unsigned int fib(unsigned int n)
+ {
+   if (n <= 1)
+    {
+      return n;
+    }
+  return fib(n-1) + fib(n-2);
 }
 
-int factorial(int n)
-{
-  //
-
-  return 0;
-}
-
-int ack(int m, int n)
-{
-  //
-
-  return 0;
-}
-
-TEST_CASE("Fibonacci Sequence", "[Fibonacci]")
-{
-    REQUIRE(fib(0) == 0);
-    REQUIRE(fib(1) == 1);
-    REQUIRE(fib(9) == 34);
-    REQUIRE(fib(14) == 377);
+TEST_CASE( "Fibonacci Sequence", "[fibonacci]" ) {
+    REQUIRE( fib(0) == 0 );
+    REQUIRE( fib(1) == 1 );
+    REQUIRE( fib(9) == 34 );
+    REQUIRE( fib(14) == 377 );
 }
