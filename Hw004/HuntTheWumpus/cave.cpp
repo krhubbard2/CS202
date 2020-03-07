2871ad01688dcac0b8d285635243fcddd94e9f69
Kelby <krhubbard2@alaska.edu>
2020-03-06 22:18:43 -0900

    Implamented Cave.cpp

--- Hw004/HuntTheWumpus/cave.cpp
+++ Hw004/HuntTheWumpus/cave.cpp
@@ -3,3 +3,44 @@
 // March 8, 2020
 // Hw004 -- Hunt the Wumpus
 #include "cave.hpp"
+
+Cave::Cave() {}
+
+Cave::Cave(int room)
+{
+  _room = room;
+}
+
+int Cave::getRoom() const
+{
+  return _room;
+}
+int Cave::getRoomOne() const
+{
+  return _one;
+}
+
+int Cave::getRoomTwo() const
+{
+  return _two;
+}
+
+int Cave::getRoomThree() const
+{
+  return _three;
+}
+
+void Cave::setRoomOne(const Cave& room)
+{
+  _one = room.getRoom();
+}
+
+void Cave::setRoomTwo(const Cave& room)
+{
+  _two = room.getRoom();
+}
+
+void Cave::setRoomThree(const Cave& room)
+{
+  _three = room.getRoom();
+}
