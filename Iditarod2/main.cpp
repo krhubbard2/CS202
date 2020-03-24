614f153a9f49029710ddf75ce3feca0c306f827a
Kelby <krhubbard2@alaska.edu>
2020-03-24 14:19:54 -0800

    Test largest numbers machine can handle

--- Iditarod2/main.cpp
+++ Iditarod2/main.cpp
@@ -30,7 +30,23 @@ int main()
 {
   StopWatch stopwatch;
   stopwatch.starttimer();
-  cout << ack(1,1) << endl;
+  cout << ack(3,13) << endl;
   stopwatch.stoptimer();
   stopwatch.elapsed();
+
+
+  //Results
+  //(3,3) = 61 : Elapsed time: 0.000015971s
+  //(3,4) = 125 : Elapsed time: 0.000074735s
+  //(3,5) = 253 : Elapsed time: 0.000263427s
+  //(3,10) = 8189 : Elapsed time: 0.162673s
+  //(3,12) = 32765 : Elapsed time: 2.23392s
+  //(3,13) = 65533 : Elapsed time: 9.18236s
+  //(4,1)= 65533 : Elapsed time: 9.51603s
+  //(3,14) = 131069 : Elapsed time: 37.7826s
+  //(2,1000000) = 200003 : Elapsed time: 71.1964s
+  //(3,15) : Segmentation fault (core dumped)
+  //(4,2) : Segmentation fault (core dumped)
+
+
 }
