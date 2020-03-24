8a8995f151b77137c47e14b651dbc08ea803a929
Kelby <krhubbard2@alaska.edu>
2020-03-24 12:40:06 -0800

    Write fib_loop(n)

--- Iditarod1/main.cpp
+++ Iditarod1/main.cpp
@@ -8,7 +8,7 @@
 
 //Design
 // [X] fib(n)
-// [] fib_loop(n)
+// [X] fib_loop(n)
 // [] factorial(n)
 // [] factorial_loop(n)
 
@@ -22,6 +22,24 @@ unsigned int fib(unsigned int n)
   return fib(n-1) + fib(n-2);
 }
 
+//Fibonacci without recursion
+int fib_loop(int n)
+{
+  if (n <= 1)
+  {
+    return n;
+  }
+  int a = 1;
+  int b = 1;
+  for (int i = 2; i < n; ++i)
+  {
+    int temp = a;
+    a += b;
+    b = temp;
+  }
+  return a;
+}
+
 
 TEST_CASE( "Fibonacci Sequence", "[fibonacci]" )
 {
@@ -32,4 +50,13 @@ TEST_CASE( "Fibonacci Sequence", "[fibonacci]" )
     REQUIRE( fib(9) == 34 );
     REQUIRE( fib(14) == 377 );
   }
+
+  SECTION ("FIBONACCI NON RECURSIVE")
+  {
+    REQUIRE( fib_loop(0) == 0);
+    REQUIRE( fib_loop(1) == 1 );
+    REQUIRE( fib_loop(9) == 34 );
+    REQUIRE( fib_loop(14) == 377 );
+  }
+
 }
