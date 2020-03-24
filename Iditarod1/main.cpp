59d4cca9316140c00ea8e66aa970374ad3701c12
Kelby <krhubbard2@alaska.edu>
2020-03-24 12:35:56 -0800

    Design layout made.

---
+++ Iditarod1/main.cpp
@@ -0,0 +1,24 @@
+// Kelby Hubbard
+// CS202
+// March 24, 2020
+// Iditarod Challenge 1
+
+#include <iostream>
+
+//Design
+// [] fib(n)
+// [] fib_loop(n)
+// [] factorial(n)
+// [] factorial_loop(n)
+
+int main()
+{
+
+
+
+
+
+
+
+  return 0;
+}
