f742f9dad1f2702584e184dcff750d03ae179cd9
Kelby <krhubbard2@alaska.edu>
2020-03-24 12:38:39 -0800

    Write fib(n)

--- Iditarod1/main.cpp
+++ Iditarod1/main.cpp
@@ -3,22 +3,33 @@
 // March 24, 2020
 // Iditarod Challenge 1
 
-#include <iostream>
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
 
 //Design
-// [] fib(n)
+// [X] fib(n)
 // [] fib_loop(n)
 // [] factorial(n)
 // [] factorial_loop(n)
 
-int main()
+//Fibonacci with recursion
+unsigned int fib(unsigned int n)
+ {
+   if (n <= 1)
+    {
+      return n;
+    }
+  return fib(n-1) + fib(n-2);
+}
+
+
+TEST_CASE( "Fibonacci Sequence", "[fibonacci]" )
 {
-
-
-
-
-
-
-
-  return 0;
+  SECTION("FIBONACCI RECURSIVE")
+  {
+    REQUIRE( fib(0) == 0 );
+    REQUIRE( fib(1) == 1 );
+    REQUIRE( fib(9) == 34 );
+    REQUIRE( fib(14) == 377 );
+  }
 }
