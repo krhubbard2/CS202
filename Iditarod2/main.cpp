cae9a82432772001c06179ac261826e7e621875b
Kelby <krhubbard2@alaska.edu>
2020-03-24 14:01:22 -0800

    Write ack(m,n)

---
+++ Iditarod2/main.cpp
@@ -0,0 +1,30 @@
+// Kelby Hubbard
+// CS202
+// March 24, 2020
+// Iditarod Challenge 2
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+//Ackermann's Function
+int ack(int m, int n)
+{
+  if (m == 0)
+  {
+    return n + 1;
+  }
+  else if ((m > 0) && (n == 0))
+  {
+    return ack(m - 1, 1);
+  }
+  else if ((m > 0) && (n > 0))
+  {
+    return ack(m - 1, ack(m, n-1));
+  }
+};
+
+int main()
+{
+  cout << ack(1,1) << endl;
+}
