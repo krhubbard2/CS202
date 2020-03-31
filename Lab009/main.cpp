100a36766b0577a9f7237f01c46de0d3590bdda0
Kelby <krhubbard2@alaska.edu>
2020-03-31 11:57:14 -0800

    Static global variable

--- Lab009/main.cpp
+++ Lab009/main.cpp
@@ -9,14 +9,41 @@ using std::endl;
 #include <iomanip>
 #include <string>
 
-// [] Static local variable
-// [] Static global variable
+// [X] Static local variable
+// [X] Static global variable
 // [] Static member variable
 // [] Static member function
+
+static int test_static_count = 0;
+
+class TestStatic
+{
+public:
+  TestStatic() :
+    instanceNum_(test_static_count) {
+    cout << __FUNCTION__ << " " << instanceNum_ << " initialized." << endl;
+    test_static_count++;
+  }
+
+  ~TestStatic()
+  {
+    cout << __FUNCTION__ << " " << instanceNum_ << " destroyed." << endl;
+  }
+
+private:
+  int instanceNum_;
+};
+
+namespace
+{
+  TestStatic ts1;
+  TestStatic ts2;
+}
 
 void foo_static()
 {
   static int count = 0;
+  static TestStatic ts;
   if (!count)
   {
     cout << __FUNCTION__ << " is not initialized" << endl;
@@ -31,9 +58,11 @@ void foo_static()
 
 int main(int argc, char** argv)
 {
+  cout << "Starting main" << endl;
   foo_static();
   foo_static();
   foo_static();
+  cout << "Leaving main" << endl;
 
   return 0;
 }
