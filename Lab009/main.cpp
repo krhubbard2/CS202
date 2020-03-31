c5a88af57843f4dc1c2aaf2fae4e71a0b1991063
Kelby <krhubbard2@alaska.edu>
2020-03-31 11:45:44 -0800

    Start initial program lab009

---
+++ Lab009/main.cpp
@@ -0,0 +1,20 @@
+// Kelby Hubbard
+// CS202
+// Mar. 31, 2020
+// Lab009
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+// [] Static local variable
+// [] Static global variable
+// [] Static member variable
+// [] Static member function
+
+int main(int argc, char** argv)
+{
+
+
+  return 0;
+}
