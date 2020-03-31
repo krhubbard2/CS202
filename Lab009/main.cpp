7beeda0cbe2a4e89fb63f714bd6ea92f232620d2
Kelby <krhubbard2@alaska.edu>
2020-03-31 11:50:36 -0800

    Static local variable

--- Lab009/main.cpp
+++ Lab009/main.cpp
@@ -4,6 +4,8 @@
 // Lab009
 
 #include <iostream>
+using std::cout;
+using std::endl;
 #include <iomanip>
 #include <string>
 
@@ -12,9 +14,26 @@
 // [] Static member variable
 // [] Static member function
 
+void foo_static()
+{
+  static int count = 0;
+  if (!count)
+  {
+    cout << __FUNCTION__ << " is not initialized" << endl;
+    count = 1;
+  }
+  else
+  {
+    cout << __FUNCTION__ << " called " << count << " times" << endl;
+    count++;
+  }
+}
+
 int main(int argc, char** argv)
 {
-
+  foo_static();
+  foo_static();
+  foo_static();
 
   return 0;
 }
