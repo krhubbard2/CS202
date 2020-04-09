707e85feafdf2c0122d82dd55977cff8daaeedda
Kelby <krhubbard2@alaska.edu>
2020-04-09 13:48:09 -0800

    Default base class object

--- Lab011/main.cpp
+++ Lab011/main.cpp
@@ -8,14 +8,30 @@ using std::cout;
 using std::endl;
 
 // Design
-// [] Default base class object
+// [X] Default base class object
 // [] Base class object using 1 paremeter constructor
 // [] Default dervied class object
 // [] Dervied class object using 2 paremeter constructor
 
+////////////////////////////////////////////////////////////////////////
+// B A S E    C L A S S ////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+
+class Base
+{
+public:
+  Base(){
+     cout << "Constructing default Base object. Initial value is " << mph;}
+
+private:
+  int mph{60};
+};
 
 int main(int argc, char** argv)
 {
+
+  {Base b;}
+  cout << endl;
 
 
 
