f5107dd15deb0e2daa20599feef1a157f9010054
Kelby <krhubbard2@alaska.edu>
2020-04-16 12:10:29 -0800

    Lab012: initialize project

---
+++ Lab012/main.cpp
@@ -0,0 +1,24 @@
+// Kelby Hubbard
+// CS 202
+// April 14, 2020
+// Lab012 -- Polymorphism
+
+#include <iostream>
+#include <string>
+
+// [] Write Base class with two methods (one virtual)
+// [] Write a Derived class with two methods (one virtual)
+// [] Test base class pointer to base class object
+// [] Test base class pointer to derived class object
+// [] Test derived class pointer to derived class object
+// [] Test base class reference to base class object
+// [] Test base class reference to derived object
+// [] Test derived class reference to derived class object
+
+int main(int argc, char** argv)
+{
+
+
+
+  return 0;
+}
