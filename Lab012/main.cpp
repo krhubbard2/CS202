dd05ec0784fbfd4930ad0879e7ead3cb1dfdb4e2
Kelby <krhubbard2@alaska.edu>
2020-04-16 12:17:07 -0800

    Write a Derived class with two methods (one virtual)

--- Lab012/main.cpp
+++ Lab012/main.cpp
@@ -8,7 +8,7 @@ using std::cout;
 #include <string>
 
 // [X] Write Base class with two methods (one virtual)
-// [] Write a Derived class with two methods (one virtual)
+// [X] Write a Derived class with two methods (one virtual)
 // [] Test base class pointer to base class object
 // [] Test base class pointer to derived class object
 // [] Test derived class pointer to derived class object
@@ -19,18 +19,31 @@ using std::cout;
 class Base{
 public:
   Base() { cout << "Base::Base() constructor\n"; }
-  ~Base() { cout << "Base::Base() destructor\n"; }
+  virtual ~Base() { cout << "~Base::Base() destructor\n"; }
 
   void functionA() { cout << "Base::functionA() non-virtual\n"; }
   virtual void functionB() { cout << "Base::functionB() virtual\n"; }
 };
 
+class Derived : public Base{
+public:
+  Derived() { cout << "Derived::Derived() constructor\n"; }
+  ~Derived() { cout << "~Derived::Derived() destructor\n"; }
+
+  void functionA() { cout << "Derived::functionA() non-virtual\n"; }
+  virtual void functionB() { cout << "Derived::functionB() virtual\n"; }
+};
+
 void dashednewline(){
   cout << "--------------------\n";
 }
+
 int main(int argc, char** argv)
 {
   { Base b; b.functionA(); b.functionB();}
+  dashednewline();
+
+  { Derived d; d.functionA(); d.functionB();}
   dashednewline();
 
 
