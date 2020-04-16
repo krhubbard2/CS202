e401878c3553593edcfa883ede1d2d4b7459e10d
Kelby <krhubbard2@alaska.edu>
2020-04-16 12:13:40 -0800

    Write Base class with two methods (one virtual)

--- Lab012/main.cpp
+++ Lab012/main.cpp
@@ -4,9 +4,10 @@
 // Lab012 -- Polymorphism
 
 #include <iostream>
+using std::cout;
 #include <string>
 
-// [] Write Base class with two methods (one virtual)
+// [X] Write Base class with two methods (one virtual)
 // [] Write a Derived class with two methods (one virtual)
 // [] Test base class pointer to base class object
 // [] Test base class pointer to derived class object
@@ -15,9 +16,22 @@
 // [] Test base class reference to derived object
 // [] Test derived class reference to derived class object
 
+class Base{
+public:
+  Base() { cout << "Base::Base() constructor\n"; }
+  ~Base() { cout << "Base::Base() destructor\n"; }
+
+  void functionA() { cout << "Base::functionA() non-virtual\n"; }
+  virtual void functionB() { cout << "Base::functionB() virtual\n"; }
+};
+
+void dashednewline(){
+  cout << "--------------------\n";
+}
 int main(int argc, char** argv)
 {
-
+  { Base b; b.functionA(); b.functionB();}
+  dashednewline();
 
 
   return 0;
