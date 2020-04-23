4cf215c9a272afd7153d11c3830ecd2dce57a80c
Kelby <krhubbard2@alaska.edu>
2020-04-23 11:11:21 -0800

    Lab014 start project

---
+++ Lab014/main.cpp
@@ -0,0 +1,28 @@
+// Kelby Hubbard
+// CS202
+// April 23, 2020
+// Lab014
+
+#include <iostream>
+using std::cout;
+#include <string>
+using std::string;
+
+// [] Write function template
+// [] Test with int and double
+// [] Test with a string
+// [] Test with a C string
+
+
+int main(int argc, char** argv){
+
+
+
+
+
+
+
+
+
+  return 0;
+}
