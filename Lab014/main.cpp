dd5ab341774ddcdbb5df07f041fbff6d3efe1bf3
Kelby <krhubbard2@alaska.edu>
2020-04-23 11:16:29 -0800

    Test with a C string

--- Lab014/main.cpp
+++ Lab014/main.cpp
@@ -12,17 +12,22 @@ using std::string;
 // [X] Write function template 'twice'
 // [X] Test with int and double
 // [X] Test with a C++ string
-// [] Test with a C string
+// [X] Test with a C string
 
 template <typename T>
 T twice(const T& x){
   return x + x;
 }
 
+string twice(const char* x){
+  return twice(string(x));
+}
+
 int main(int argc, char** argv){
   cout << twice(2) << endl;
   cout << twice(2.3) << endl;
   cout << twice(string("Hello")) << endl;
+  cout << twice("Hello") << endl;
 
 
 
