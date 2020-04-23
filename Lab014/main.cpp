65df6f513f9fe77e0750338554d5e721bdadb768
Kelby <krhubbard2@alaska.edu>
2020-04-23 11:12:59 -0800

    Write function template 'twice'

--- Lab014/main.cpp
+++ Lab014/main.cpp
@@ -5,16 +5,23 @@
 
 #include <iostream>
 using std::cout;
+using std::endl;
 #include <string>
 using std::string;
 
-// [] Write function template
+// [] Write function template 'twice'
 // [] Test with int and double
-// [] Test with a string
+// [] Test with a C++ string
 // [] Test with a C string
 
+template <typename T>
+T twice(const T& x){
+  return x + x;
+}
 
 int main(int argc, char** argv){
+  cout << twice(2) << endl;
+  cout << twice(2.3) << endl;
 
 
 
